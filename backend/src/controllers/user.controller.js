594ddd8a428925559f4ada99c017b5f7686d067f
Saksham kushwah <eashuop91@gmail.com>
2025-10-12 00:15:44 +0530

    recommended users routs done

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,26 @@
-export async function getRecommendedUsers(req, res) {};
+import User from "../models/User.js";
+
+
+export async function getRecommendedUsers(req, res) {
+
+    try {
+        const currentUserId = req.user._id;
+        const currentUser = req.user;
+
+        const getRecommendedUsers = await User.find({
+            $and: [
+                {_id : {$ne: currentUserId}}, //exclude current user 
+                {$id : {$nin: currentUser.friends}}, // exclude friends
+                {isOnboarded: true} // only onboarded users
+            ]
+        })
+        res.status(200).json(recommendUsers);
+    } catch (error) {
+        console.log("Error in getRecommendedUsers controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+        
+    }
+};
 
 
 export async function getMyFriends(req, res) {};
