dda9dcfd65ef2f14ca875ef17528eaecb8df6976
Saksham kushwah <eashuop91@gmail.com>
2025-10-12 01:06:08 +0530

    send friend req Done

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/User.js";
 
 
@@ -37,3 +38,51 @@ export async function getMyFriends(req, res) {
         
     }
 };
+
+export async function sendFriensRequest(req, res) {
+
+    try {
+         const myId = req.user.id;
+         const { id: recipientId } = req.params;
+
+         //prevents sendingng request to yourself
+         if(myId === recipientId){
+            return res.status(400).json({ message: "You cannot send friend request to yourself"});
+         }
+
+         const recipient = await User.findById(recipientId);
+            if(!recipient){
+                return res.status(404).json({ message: "Recipient user not found"});
+            }
+
+            // Check if they are already friends
+            if(recipient.friends.includes(myId)){
+                return res.status(400).json({ message: "You are already friends with this user"});
+            }
+
+            //check if a req already exists
+            const existingRequest = await FriendRequest.findOne({
+                $or: [
+                    { sender: myId, recipient: recipientId },
+                    { sender: recipientId, recipient: myId }
+                ],
+            });
+
+            if(existingRequest){
+                return res
+                .status(400)
+                .json({ message: "A friend request already exists between you and this user"});
+            }
+
+
+            const friendRequest = await FriendRequest.create({
+                sender: myId,
+                recipient: recipientId,
+            });
+
+            res.status(201).json(friendRequest);
+    } catch (error) {
+        console.log("Error in sendFriendRequest controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
