830d4ccd65ed0c1fdab4c70bf2ef5dac8c37c14d
Saksham kushwah <eashuop91@gmail.com>
2025-10-25 23:20:13 +0530

    home page & notification page done

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -9,9 +9,8 @@ export const login = async (loginData) => {
   const response = await axiosInstance.post("/auth/login", loginData);
   return response.data;
 };
-
-export const logout = async (logoutData) => {
-  const response = await axiosInstance.post("/auth/logout", logoutData);
+export const logout = async () => {
+  const response = await axiosInstance.post("/auth/logout");
   return response.data;
 };
 
@@ -37,5 +36,30 @@ export async function getUserFriends() {
 
 export async function getRecommendedUsers() {
   const response = await axiosInstance.get("/users");
+  return response.data;
+}
+
+export async function getOutgoingFriendReqs() {
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
+
+export async function sendFriendRequest(userId) {
+  const response = await axiosInstance.post(`/users/friend-request/${userId}`);
+  return response.data;
+}
+
+export async function getFriendRequests() {
+  const response = await axiosInstance.get("/users/friend-requests");
+  return response.data;
+}
+
+export async function acceptFriendRequest(requestId) {
+  const response = await axiosInstance.put(`/users/friend-request/${requestId}/accept`);
+  return response.data;
+}
+
+export async function getStreamToken() {
+  const response = await axiosInstance.get("/chat/token");
   return response.data;
 }
